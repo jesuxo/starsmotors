0858211653c7d589ab8b63b0f61046c854e0a77b
Jesus Celis <geal16ster@gmail.com>
2026-07-15 07:17:51 -0400

    asd

--- public/build/js/backend/edit-product.init.js
+++ public/build/js/backend/edit-product.init.js
@@ -20,27 +20,6 @@ Array.prototype.slice.call(forms).forEach(function (form) {
 
             var productCategoryValue = productCategoryInput.getValue(true);
 
-            var codprod    = document.getElementById("codprod").value;
-            var descrip    = document.getElementById("descrip").value;
-            var descrip2   = document.getElementById("descrip2").value;
-            var descrip3   = document.getElementById("descrip3").value;
-
-            var refere     = document.getElementById("refere").value;
-            var marca      = document.getElementById("marca").value;
-            var codinst    = productCategoryInput.getValue(true);
-            var isadmin    = document.getElementById("isadmin").value;
-
-            if(isadmin == 1){
-                var preciod    = document.getElementById("preciod").value;
-                var costod     = document.getElementById("costod").value;
-                var costod2    = document.getElementById("costod2").value;
-                var costod3    = document.getElementById("costod3").value;
-                var esexento   = document.getElementById("esexento").value;
-            }
-
-            var exdecimal  = document.getElementById("exdecimal").value;
-            var unidad     = document.getElementById("unidad").value;
-
             var formAction = document.getElementById("formAction").value;
 
             if (formAction == "edit" && productCategoryValue !== ""  ) {
